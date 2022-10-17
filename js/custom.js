d70d0113b6babeae12f2cd0bac81faeb80a10942
loyicapages <111897079+loyicapages@users.noreply.github.com>
2022-10-17 20:19:09 +0500

    JS update

--- js/custom.js
+++ js/custom.js
@@ -13,3 +13,25 @@ $(window).scroll(function() {
   $('#navbarSupportedContent').toggleClass('d-block');
   
   })
+
+  
+  $(document).ready(function() {
+    // get video source from data-src button
+    var $videoSrc;  
+    $('.video-btn').click(function() {
+        $videoSrc = $(this).data( "src" );
+        console.log($videoSrc);
+    });
+    
+    // autoplay video on modal load  
+    $('#liquidityVideo').on('shown.bs.modal', function (e) {
+    // youtube iframe configuration and settings
+    $("#video").attr('src',$videoSrc + "?autoplay=1&rel=0&controls=0&loop=1&modestbranding=0"); 
+    })
+    // stop playing the youtube video when modal closed
+    $('#liquidityVideo').on('hide.bs.modal', function (e) {
+        // stop video
+        $("#video").attr('src',$videoSrc); 
+    }) 
+    });
+    
